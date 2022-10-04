da94e9aa324e5d32741925ab64f8f1f6ea23c207
mylesmurphy3207 <112994560+mylesmurphy3207@users.noreply.github.com>
2022-10-04 15:33:08 -0700

    calculator 10/04 assignment 3

---
+++ calculator.js
@@ -0,0 +1,45 @@
+function Calculator(){
+    
+    console.log("Calculating");
+    let num1 = Number(prompt("Enter num1:"));
+    let num2 = Number(prompt("Enter num2:"));
+    sum(num1,num2);   
+}
+function Calculator(){
+    
+    console.log("Calculating");
+    let num1 = Number(prompt("Enter num1:"));
+    let num2 = Number(prompt("Enter num2:"));
+    mult(num1,num2);   
+}
+function Calculator(){
+    
+    console.log("Calculating");
+    let num1 = Number(prompt("Enter num1:"));
+    let num2 = Number(prompt("Enter num2:"));
+    div(num1,num2);   
+}
+function Calculator(){
+    
+    console.log("Calculating");
+    let num1 = Number(prompt("Enter num1:"));
+    let num2 = Number(prompt("Enter num2:"));
+    sub(num1,num2);   
+}
+
+function sum(a,b){
+    let total = a + b;
+    document.getElementById("sumRes").innerHTML= `<p> ${a} + ${b} = ${total}</p>`;
+}
+function mult(a,b){
+    let total = a * b;
+    document.getElementByID("mulRes").innerHTML=`<P> ${a} + ${b} = ${total}</p>`;
+}
+function div(a,b){
+    let total = a / b;
+    document.getElementById("divRes").innerHTML=`<p> ${a} + ${b} = ${total}</p>`;
+}
+function sub(a,b){
+    let total = a - b;
+    document.getElementByID("subRes").innerHTML=`<p> ${a} + ${b} = ${total}</p>`;
+}
